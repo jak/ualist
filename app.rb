2a83e332489aa29a8c0634df9924aa8d615e22b5
Jak Spalding <jak.spalding@gmail.com>
2012-11-14 23:28:41 +0000

    Printing out the useragent

--- app.rb
+++ app.rb
@@ -1,5 +1,22 @@
 require 'sinatra'
+require 'haml'
 
-get '/hi' do
-  "Hello World!"
+get '/wut' do
+  redirect 'https://github.com/jakspalding/ualist'
 end
+
+get '/' do
+  @useragent = request.user_agent
+  @useragents = []
+  haml :index
+end
+
+__END__
+
+@@ index
+%html
+  %h1=@useragent
+  %ul
+  - @useragents.each do |useragent|
+    %li= useragent
+  %a{:href => url('/wut')} What is this?
